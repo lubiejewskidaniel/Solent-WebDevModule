798aa0846dfa01b718ece5e0a39e4b3be470b29d
lubiejewskidaniel <lubiejewskidaniel@gmail.com>
2024-03-15 11:16:53 +0000

    FAQ Section done

--- Faq/js/count1.js
+++ Faq/js/count1.js
@@ -1,12 +1,11 @@
-// JavaScript to handle the click events and update cont2
 document.addEventListener("DOMContentLoaded", function () {
 	var cont1Links = document.querySelectorAll("#cont1 a");
 
 	cont1Links.forEach(function (link) {
 		link.addEventListener("click", function (event) {
-			event.preventDefault(); // Prevent default link behavior
-			var content = this.getAttribute("data-content"); // Get content from data attribute
-			document.getElementById("cont2").innerHTML = content; // Update cont2 content
+			event.preventDefault();
+			var content = this.getAttribute("data-content");
+			document.getElementById("cont2").innerHTML = content;
 		});
 	});
 });
