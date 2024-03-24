e22b39575af512237bee9ba89e5d0eb36a44616e
lubiejewskidaniel <lubiejewskidaniel@gmail.com>
2024-03-24 23:00:46 +0000

    trying to use db file

--- Python DB/week5.py
+++ Python DB/week5.py
@@ -1,7 +1,7 @@
 import sqlite3 #allows our python program to interact with SQLite DB
 
 #Connection to our database
-conn = sqlite3.connect('assessment.db')
+conn = sqlite3.connect('/Python DB/nowa.db')
 
 cursor = conn.cursor()#Enters the DB and interacts with it.
 
@@ -9,3 +9,6 @@ results = cursor.execute('Select * from categories')
 
 for row in results:
     print(row)
+
+cursor.close()
+conn.close()
